138f71c5a0d1e8caf465879cb32ad4f1477f936e
Tony SC <tonysolano08@gmail.com>
2023-08-18 12:08:34 -0600

    Archivos subidos

---
+++ src/track/Obstaculo.java
@@ -0,0 +1,14 @@
+package track;
+
+public class Obstaculo {
+	
+	private String tipo;
+	private int largo;
+	private int daño;
+	
+	public Obstaculo(String pTipo, int pLargo, int pDaño) {
+		this.tipo = pTipo;
+		this.largo = pLargo;
+		this.daño = pDaño;
+	}
+}
